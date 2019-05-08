084e813b36804659917c5664ea67ea0c5b9a87f1
Sean Kennealy <spkennealy@gmail.com>
2019-05-08 00:27:27 -0700

    One last attempt at the heroku seeding issue

--- db/migrate/20190503170700_add_columns_to_playlist_followers.rb
+++
@@ -1,9 +0,0 @@
-# class AddColumnsToPlaylistFollowers < ActiveRecord::Migration[5.2]
-#   def change
-#     add_column :playlist_followers, :follower_id, :integer, null: false
-#     add_column :playlist_followers, :playlist_id, :integer, null: false
-    
-#     add_index :playlist_followers, [:follower_id, :playlist_id], unique: true
-#     add_index :playlist_followers, :playlist_id
-#   end
-# end
