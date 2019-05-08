401f3954d3745af49bd34f9e764433c6cbf89384
Sean Kennealy <spkennealy@gmail.com>
2019-05-08 00:22:29 -0700

    A 2nd try at fixing heroku seeding issue

--- db/migrate/20190503171038_add_columns_to_playlist_tracks.rb
+++
@@ -1,9 +0,0 @@
-# class AddColumnsToPlaylistTracks < ActiveRecord::Migration[5.2]
-#   def change
-#       add_column :playlist_tracks, :track_id, :integer, null: false
-#       add_column :playlist_tracks, :playlist_id, :integer, null: false
-     
-#       add_index :playlist_tracks, :track_id
-#       add_index :playlist_tracks, :playlist_id
-#   end
-# end
