0d38f8e766e9689eacb006a8334f182102607f33
Sean Kennealy <spkennealy@gmail.com>
2019-06-10 16:26:30 -0700

    Add search container component giving all search utils

--- frontend/components/search/search_container.jsx
+++ frontend/components/search/search_container.jsx
@@ -1,13 +1,19 @@
-import { logout } from '../../actions/session_actions';
 import { connect } from 'react-redux';
 import Search from './search';
+import {
+    fetchPlaylistByTitle, fetchAlbumByTitle, fetchArtistByName,
+    fetchTrackByTitle
+} from '../../util/search_utils';
 
 const mapStateToProps = state => ({
-    // currentUser: state.entities.users[state.session.id]
+    currentUser: state.entities.users[state.session.id]
 });
 
 const mapDisptachToProps = dispatch => ({
-    // logout: () => dispatch(logout())
+    fetchPlaylistByTitle: title => dispatch(fetchPlaylistByTitle(title)),
+    fetchAlbumByTitle: title => dispatch(fetchAlbumByTitle(title)),
+    fetchArtistByName: name => dispatch(fetchArtistByName(name)),
+    fetchTrackByTitle: title => dispatch(fetchTrackByTitle(title))
 });
 
 export default connect(mapStateToProps, mapDisptachToProps)(Search);
